8230fb774d5b78f5b7872fe761cff4ea736a9fb3
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2022-12-23 09:40:01 +0200

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app_alex_c7_fri/ui/register/register_screen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        RegisterScreen.routeName : (context) => RegisterScreen()
+      },
+      initialRoute: RegisterScreen.routeName,
+    );
+  }
+}
